aaf2aa7210ff34a8db26cd800de9032c2f336543
Wesley Castilho <wesley.castilho8@gmail.com>
2021-02-14 22:51:33 -0300

    Aula 1 - exemplo 1

---
+++ aula1/helloWorld.kts
@@ -0,0 +1,14 @@
+/**
+ * You can edit, run, and share this code.
+ * play.kotlinlang.org
+ */
+
+fun main() {
+    println("Hello, world!!!")
+    println("---------------")
+    println(Int.MAX_VALUE)
+    println(Float.MAX_VALUE)
+    println(Long.MAX_VALUE)
+    println(Byte.MAX_VALUE)
+    println(Short.MAX_VALUE)
+}
